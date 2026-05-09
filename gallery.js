4b95b09c4f499a976ab773ecca70c3ffa8ae85a1
KaiTel <yosefnt1@gmail.com>
2026-05-09 17:30:26 +0300

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,19 @@
+const galleryData = {
+    
+    welcomeMessage: "Happy birthday",
+    endMessage: "",
+    endImage: "",
+    
+    pepole: [
+        {
+        "name": "",
+        "image": "",
+        "caption": ""
+        },
+        {
+        "name": "",
+        "image": "",
+        "caption": ""
+        }
+    ]
+}
