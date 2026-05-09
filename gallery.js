1974a6ae9357f7b8e564706d61bb51c512c61c52
KaiTel <yosefnt1@gmail.com>
2026-05-09 17:40:48 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,19 +1,19 @@
 const galleryData = {
-    
-    welcomeMessage: "Happy birthday",
-    endMessage: "",
-    endImage: "",
-    
-    pepole: [
-        {
-        "name": "",
-        "image": "",
-        "caption": ""
-        },
-        {
-        "name": "",
-        "image": "",
-        "caption": ""
-        }
-    ]
-}
+  welcomeMessage: "Happy birthday",
+
+  endMessage: "Thanks for watching",
+  endImage: "",
+
+  people: [
+    {
+      name: "Name 1",
+      image: "image-url-1",
+      caption: "Caption 1"
+    },
+    {
+      name: "Name 2",
+      image: "image-url-2",
+      caption: "Caption 2"
+    }
+  ]
+};
