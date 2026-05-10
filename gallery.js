e0eb378d270921807c86d7f1bf65378c838a75f0
KaiTel <yosefnt1@gmail.com>
2026-05-11 00:17:30 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,20 +1,18 @@
 const galleryData = {
-  welcomeMessage: "Happy birthday",
-  music: "https://mp3tourl.com/audio/1778104889205-d3f1bf97-6ff6-4de5-be39-7b816d0753d0.m4a",
-
-  endMessage: "Thanks for watching",
-  endImage: "https://i.imgur.com/r9n0CP2.jpeg",
 
   people: [
     {
-      name: "Name 1",
-      image: "https://i.imgur.com/6kayMGP.jpeg",
-      caption: "Caption 1"
+      image: "image1.jpg",
+      caption: "هنا الكلام اللي يظهر مع الصورة الأولى"
     },
     {
-      name: "Name 2",
-      image: "https://i.imgur.com/nDdMsyO.jpeg",
-      caption: "Caption 2"
+      image: "image2.jpg",
+      caption: "هنا الكلام للصورة الثانية"
+    },
+    {
+      image: "image3.jpg",
+      caption: "هنا الكلام للصورة الثالثة"
     }
-  ]
+  ],
+
 };
