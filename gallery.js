0832ca082465b993c8ee82769760d71e0ae2020c
KaiTel <yosefnt1@gmail.com>
2026-05-12 19:37:37 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,20 +3,14 @@ const galleryData = {
   welcomeText: "happy birthday",
   people: [
     {
-      name: "tt",
-      image: "https://i.imgur.com/Nmwgm8m.jpeg",
+      image: "https://cdn.discordapp.com/attachments/1503121205647900873/1503125804622549022/Screenshot_20260510_233821_Gallery.jpg?ex=6a0430be&is=6a02df3e&hm=1f9d18671690c2c22d17d1a8ca9aee841da511208c7b64d6f2d7bac9c3d5842b&",
       caption: "هنا الكلام اللي يظهر مع الصورة الأولى"
     },
     {
-      name: "tt",
-      image: "https://i.imgur.com/Nmwgm8m.jpeg",
+      image: "https://cdn.discordapp.com/attachments/1503121205647900873/1503125961393307861/Screenshot_20260510_233858_Gallery.jpg?ex=6a0430e3&is=6a02df63&hm=83885bfd77ece755e891423622674c109b5058d45fabeb3fb6dedf5406aa2f2b&",
       caption: "هنا الكلام للصورة الثانية"
-    },
-    {
-      name: "tt",
-      image: "https://i.imgur.com/Nmwgm8m.jpeg",
-      caption: "هنا الكلام للصورة الثالثة"
     }
+    
   ],
   
     endMessage: "endddddd",
