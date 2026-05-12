edf38f3dcc2a89edac3e7242add3e730d4e97604
KaiTel <yosefnt1@gmail.com>
2026-05-12 17:38:14 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,14 +3,17 @@ const galleryData = {
   welcomeText: "happy birthday",
   people: [
     {
+      name: "tt",
       image: "https://i.imgur.com/Nmwgm8m.jpeg",
       caption: "هنا الكلام اللي يظهر مع الصورة الأولى"
     },
     {
+      name: "tt",
       image: "https://i.imgur.com/Nmwgm8m.jpeg",
       caption: "هنا الكلام للصورة الثانية"
     },
     {
+      name: "tt",
       image: "https://i.imgur.com/Nmwgm8m.jpeg",
       caption: "هنا الكلام للصورة الثالثة"
     }
